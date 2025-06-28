6d78df2c9b01494d0acc895f470e96c856d02465
anishkumar <anish.km@datafoundry.ai>
2025-06-29 00:42:28 +0530

    anish

--- redish.py
+++ redish.py
@@ -16,7 +16,7 @@ for key in keys:
     print(f"Key: {key}")
 
     try:
-        # Try to decode the value as UTF-8 text
+        # Try to decode the value as UTF-8 text anish
         value = r.get(key)
         try:
             # Try parsing as JSON
